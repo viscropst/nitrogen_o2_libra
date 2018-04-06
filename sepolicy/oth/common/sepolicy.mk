0ad11cfd3d5e68722f34ca38930f79f721a337cd
viscropst <viscropme@gmail.com>
2018-04-06 19:32:18 +0800

    Fix the sepolicy and Tweak the non profile

--- sepolicy/oth/common/sepolicy.mk
+++ sepolicy/oth/common/sepolicy.mk
@@ -1,5 +1,5 @@
 BOARD_SEPOLICY_DIRS += \
    device/xiaomi/libra/sepolicy/oth/common/vendor
    
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+PLAT_PRIVATE_POLICY += \
    device/xiaomi/libra/sepolicy/oth/common/private
